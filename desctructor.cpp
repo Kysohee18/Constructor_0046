ceed969418d5177fe0dc588072468d7094fe4db1
Kyosohee18 <hanashahky@gmail.com>
2025-05-17 17:08:46 +0700

    member function

--- desctructor.cpp
+++ desctructor.cpp
@@ -11,3 +11,9 @@ class angka {
         void cetakData();
         void isiData();
 };
+//definisi member fuction 
+angka::angka(int i){ // contructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}

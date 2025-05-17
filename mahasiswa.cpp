449599dadc3f87055872b251feba6352838bf8e5
Kyosohee18 <hanashahky@gmail.com>
2025-05-17 16:57:52 +0700

    class buku dan friend class petugas

--- mahasiswa.cpp
+++ mahasiswa.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Buku {
+private:
+    string judul;
+    string penulis;
+    bool dipinjam;
+
+public:
+    Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
+
+    // agar hanya bisa dipanggil oleh friend class Petugas
+    void tampilInfo() const {
+        cout << "Judul: " << judul << ", Penulis: " << penulis 
+             << ", Status: " << (dipinjam ? "Dipinjam" : "Tersedia") << endl;
+    }
+
+    friend class Petugas; // Hanya Petugas boleh ubah status pinjam
+};
